da5bed7a5e0c20b21cdb3badadf7412dc54f30b3
thomas <thomas.bennett@iostudio.com>
2012-06-20 17:14:07 -0500

    category images

--- blog/wp-content/themes/encore/sidebar.php
+++ blog/wp-content/themes/encore/sidebar.php
@@ -20,55 +20,33 @@
     <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/social/facebook.jpg" alt="Facebook" /></a></li>
     <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/social/twitter.jpg" alt="Twitter" /></a></li>
     <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/social/pinterest.jpg" alt="Pinterest" /></a></li>
-    <li><a href="/feed"><img src="<?php bloginfo('template_directory'); ?>/images/social/rss.jpg" alt="RSS" /></a></li>
+    <li><a href="<?php bloginfo('rss2_url') ?>"><img src="<?php bloginfo('template_directory'); ?>/images/social/rss.jpg" alt="RSS" /></a></li>
   </ul>
   <div class="clear"></div>
 
   <?php get_search_form(); ?>
 
-    <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('Sidebar Widgets')) : else : ?>
-    
-        <!-- All this stuff in here only shows up if you DON'T have any widgets active in this zone -->
+  <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('Sidebar Widgets')) : else : ?>
+  <?php endif; ?>
+  
+  <div class="clear"></div>
 
-    	<?php //get_search_form(); ?>
-    
-    	<?php //wp_list_pages('title_li=<h2>Pages</h2>' ); ?>
+  <h2>Categories</h2>
+  <ul>
+   <?php foreach(get_categories() as $cat): ?>
+    <li>
+      <img src="<?php echo z_taxonomy_image_url($cat->term_id); ?>" />
+      <a href="<?php echo get_category_link($cat->term_id); ?>"><?php echo $cat->cat_name; ?></a>
+    </li>
+   <?php endforeach; ?>
+  </ul>
 
-      <div class="clear"></div>
+  <br clear="both" />
 
-      <h2>Categories</h2>
-      <ul>
-       <?php //wp_list_categories('show_count=1&title_li='); ?>
-       <?php $categories = get_categories(); foreach($categories as $category){ ?>
-         <?php if (function_exists('z_taxonomy_image_url')) echo z_taxonomy_image_url(); ?>
-       <?php } ?>
-      </ul>
+  <h2>Archives</h2>
+  <ul>
+    <?php wp_get_archives('type=monthly'); ?>
+  </ul>
 
-      <br clear="both" />
-    
-    	<h2>Archives</h2>
-    	<ul>
-    		<?php wp_get_archives('type=monthly'); ?>
-    	</ul>
-
-        <br clear="both" />
-                
-        <?php /*
-    	<h2>Meta</h2>
-    	<ul>
-    		<?php wp_register(); ?>
-    		<li><?php wp_loginout(); ?></li>
-    		<li><a href="http://wordpress.org/" title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress</a></li>
-    		<?php wp_meta(); ?>
-    	</ul>
-    	
-    	<h2>Subscribe</h2>
-    	<ul>
-    		<li><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a></li>
-    		<li><a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a></li>
-    	</ul>
-        */ ?>
-	
-	<?php endif; ?>
-
+  <br clear="both" />
 </div>
